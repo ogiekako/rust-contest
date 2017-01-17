fbb0958df8b917545241b42b740fe34cff29b9ae
oka <ogiekako@gmail.com>
2017-01-17 18:49:58 +0000

    Add tests to math.

--- src/math.rs
+++ src/math.rs
@@ -1,9 +1,22 @@
-/// math library.
+//! math library.
 
+/// ```
+/// use contest::math;
+/// assert_eq!(3, math::inv(2,5));
+/// assert_eq!(1, math::inv(1,2));
+/// assert_eq!(1, math::inv(3,2));
+/// ```
 pub fn inv(value: i32, modulo_prime: i32) -> i32 {
     return pow_mod(value, modulo_prime - 2, modulo_prime);
 }
 
+/// ```
+/// use contest::math;
+/// assert_eq!(8, math::pow_mod(2,3,10));
+/// assert_eq!(1, math::pow_mod(3,4,10));
+/// assert_eq!(1, math::pow_mod(0,0,3));
+/// assert_eq!(0, math::pow_mod(0,0,1));
+/// ```
 pub fn pow_mod(value: i32, exponent: i32, modulo: i32) -> i32 {
     if exponent < 0 {
         panic!("Exponent should >= 0.");
