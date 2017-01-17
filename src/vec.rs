16a3bc26b7ae1137304f02f973057f54e28a9353
oka <ogiekako@gmail.com>
2017-01-17 19:06:34 +0000

    Add tests for vec::next_permutation.

--- src/vec.rs
+++ src/vec.rs
@@ -1,4 +1,32 @@
+//! Utility methods for Vec.
 
+/// ```
+/// use contest::vec;
+/// let mut is = vec![2,3,1];
+/// assert!(vec::next_permutation(&mut is));
+/// assert_eq!(vec![3,1,2], is);
+/// assert!(vec::next_permutation(&mut is));
+/// assert_eq!(vec![3,2,1], is);
+/// assert!(!vec::next_permutation(&mut is));
+/// assert_eq!(vec![1,2,3], is);
+///
+/// let mut is = (0..3).collect();
+/// let mut count = 0;
+/// loop {
+///     count+=1;
+///     // Do something using is
+///     if !vec::next_permutation(&mut is) {
+///         break;
+///     }
+/// }
+/// assert_eq!(6, count);
+///
+/// let mut is = vec![1,2,1];
+/// assert!(vec::next_permutation(&mut is));
+/// assert_eq!(vec![2,1,1], is);
+/// assert!(!vec::next_permutation(&mut is));
+/// assert_eq!(vec![1,1,2], is);
+/// ```
 pub fn next_permutation(is: &mut Vec<i32>) -> bool {
     let n = is.len();
     for i in (1..n).rev() {
@@ -17,7 +45,7 @@ pub fn next_permutation(is: &mut Vec<i32>) -> bool {
     false
 }
 
-pub fn reverse(v: &mut Vec<i32>, from_inclusive: usize, to_exclusive: usize) {
+fn reverse(v: &mut Vec<i32>, from_inclusive: usize, to_exclusive: usize) {
     let mut i = from_inclusive;
     let mut j = to_exclusive - 1;
     while i < j {
