93749a6ad8f5b961090db4eb79212a691c758312
Keigo Oka <ogiekako@gmail.com>
2017-06-05 22:57:54 +0900

    Binary indexed tree

--- src/data/binary_indexed_tree.rs
+++ src/data/binary_indexed_tree.rs
@@ -1,31 +1,45 @@
+// Binary indexed tree
 // Referred to http://hos.ac/slides/20140319_bit.pdf
 
-pub struct BIT {
-    bit: Vec<i32>,
+use std::ops::Add;
+
+pub struct BIT<T> {
+    bit: Vec<Option<T>>,
 }
 
-impl BIT {
-    pub fn new(n: usize) -> BIT {
-        BIT { bit: vec![0;n + 1] }
+impl<'a, T: 'a> BIT<T>
+    where T: Add<Output = T> + Copy
+{
+    pub fn new(n: usize) -> BIT<T> {
+        let mut bit = BIT { bit: vec![] };
+        for i in 0..n + 1 {
+            bit.bit.push(None);
+        }
+        bit
     }
+
     // add w to the i-th element (0-origin)
-    pub fn add(&mut self, i: usize, w: i32) {
-        if i + 1 > self.bit.len() {
+    pub fn add(&'a mut self, i: usize, w: T) {
+        if i + 1 >= self.bit.len() {
             panic!("Index out of bounds: {}", i);
         }
         let mut x = (i + 1) as i32;
         while x < self.bit.len() as i32 {
-            self.bit[x as usize] += w;
+            self.bit[x as usize] = Some(self.bit[x as usize].map_or(w, |v| v + w));
             x += x & -x;
         }
     }
 
     // get the [0, i) sum.
-    pub fn sum(&self, i: usize) -> i32 {
-        let mut res = 0;
+    pub fn sum(&self, i: usize) -> Option<T> {
+        let mut res = None;
         let mut x = i as i32;
         while x > 0 {
-            res += self.bit[x as usize];
+            res = match (self.bit[x as usize], res) {
+                (Some(u), Some(v)) => Some(u + v),
+                (v, None) => v,
+                _ => res,
+            };
             x -= x & -x;
         }
         res
@@ -43,10 +57,10 @@ mod tests {
         bit.add(2, 30);
         bit.add(3, 40);
 
-        assert_eq!(0, bit.sum(0));
-        assert_eq!(10, bit.sum(1));
-        assert_eq!(30, bit.sum(2));
-        assert_eq!(60, bit.sum(3));
-        assert_eq!(100, bit.sum(4));
+        assert_eq!(None, bit.sum(0));
+        assert_eq!(Some(10), bit.sum(1));
+        assert_eq!(Some(30), bit.sum(2));
+        assert_eq!(Some(60), bit.sum(3));
+        assert_eq!(Some(100), bit.sum(4));
     }
 }
